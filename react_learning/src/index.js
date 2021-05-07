1afe4a22ee3f7a4e1d07fe845de22dffa21faf8c
Waynexia888 <qiwuwaynexia@gmail.com>
2021-05-06 19:43:10 -0700

    clock component

--- react_learning/src/index.js
+++ react_learning/src/index.js
@@ -4,12 +4,30 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
+function Clock(props) {
+  return (
+    <div>
+      <h1>Hello, world!</h1>
+      <h2>It is {props.date.toLocaleTimeString()}</h2>
+    </div>
+  )
+}
+
+
+
+function tick() {
+  ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+  <Clock date={new Date()} />
   </React.StrictMode>,
   document.getElementById('root')
 );
+}
+
+setInterval(tick, 1000);
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
